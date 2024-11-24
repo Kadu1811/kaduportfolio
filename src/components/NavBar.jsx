8ff2be19bf02bf2e33fc157d73ab949824822762
Kadu1811 <kadu.dear60@gmail.com>
2024-11-24 20:39:51 -0300

    feat: add background & start header nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,17 @@
+import logo from '../assets/raviKumarLogo.webp'
+
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">
+          <img src={logo} alt="logo" className='mx-2' width={50} height={33} />
+        </a>
+      </div>
+      <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <a href=" "></a>
+      </div>
+    </nav>
   )
 }
 
