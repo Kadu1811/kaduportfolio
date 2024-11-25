ab1cc3a0933f0c3f18d9f7db09fdea2c5b335a6a
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2024-11-25 17:00:15 -0400

    add frame motionn to contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,46 @@
 import { CONTACT } from "../constants"
+import { motion } from 'framer-motion'
 
 const Contact = () => {
     return (
         <div className="border-t border-stone-900 pb-20">
-            <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
+            <motion.h2 
+                className="my-10 text-center text-4xl"
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1 }}
+            >
+                Get in Touch
+            </motion.h2>
 
             <div className="text-center tracking-tighter">
-                <p className="my-4">
+                <motion.p 
+                    className="my-4"
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: -100 }}
+                    transition={{ duration: 1 }}
+                >
                     {CONTACT.address}
-                </p>
+                </motion.p>
 
-                <p className="my-4">
+                <motion.p 
+                    className="my-4"
+                    whileInView={{ opacity: 1, x: 0 }}
+                    initial={{ opacity: 0, x: 100 }}
+                    transition={{ duration: 1 }}
+                >
                     {CONTACT.phoneNo}
-                </p>
+                </motion.p>
 
-                <a href="#" className="border-b">
+                {/* <motion.a 
+                    href="#" 
+                    className="border-b"
+                    whileInView={{ opacity: 1, y: 0 }}
+                    initial={{ opacity: 0, y: 100 }}
+                    transition={{ duration: 1 }}
+                >
                     {CONTACT.email}
-                </a>
+                </motion.a> */}
             </div>
         </div>
     )
