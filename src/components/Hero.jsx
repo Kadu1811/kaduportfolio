e86f97b15abb5255b646c4449c722db0343a8ed7
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2024-11-25 15:43:13 -0400

    chore: install frame motion & improve hero with animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,23 @@
 import profilePic from '../assets/kadu-profile-super-cut.webp'
 import { HERO_CONTENT } from '../constants'
+import { motion } from 'framer-motion'
+
+const containerVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 0.5,
+            staggerChildren: 0.5,
+        }
+    }
+}
+
+const childVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0, transition: { duration: 0.5 }}
+}
 
 const Hero = () => {
   return (
@@ -7,33 +25,58 @@ const Hero = () => {
         <div className="flex flex-wrap lg:flex-row-reverse">
             <div className="w-full lg:w-1/2">
                 <div className="flex justify-center lg:p-8">
-                    <img src={profilePic} alt='Karlos Plentis' className='border border-stone-900 rounded-3xl' />
+                    <motion.img 
+                        src={profilePic} 
+                        alt='Karlos Plentis' 
+                        className='border border-stone-900 rounded-3xl'
+                        width={650}
+                        height={650}
+                        initial={{ x: 100, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{ duration: 1, delay: 1.5 }}
+                    />
                 </div>
             </div>
 
             <div className='w-full lg:w-1/2'>
-                <div className='flex flex-col items-center lg:items-start mt-10'>
-                    <h2 className='pb-2 text-4xl tracking-tighter lg:text-8xl'>
+                <motion.div 
+                    className='flex flex-col items-center lg:items-start mt-10'
+                    initial="hidden"
+                    animate="visible"
+                    variants={containerVariants}
+                >
+                    <motion.h2 
+                        className='pb-2 text-4xl tracking-tighter lg:text-8xl'
+                        variants={childVariants}
+                    >
                         Karlos Plentis
-                    </h2>
+                    </motion.h2>
 
-                    <span className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'>
+                    <motion.span
+                        className='bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent'
+                        variants={childVariants}
+                    >
                         Front-End Developer
-                    </span>
+                    </motion.span>
 
-                    <p className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'>
+                    <motion.p 
+                        className='my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter'
+                        variants={childVariants}
+                    >
                         {HERO_CONTENT}
-                    </p>
+                    </motion.p>
 
-                    <a href="/resume.pdf"
+                    <motion.a 
+                        href="/resume.pdf"
                         target='_blank'
                         rel='noopener noreferrer'
+                        variants={childVariants}
                         download
                         className='bg-white rounded-full p-4 text-sm text-stone-800 mb-10'
                     >
                         Download Resume
-                    </a>
-                </div>
+                    </motion.a>
+                </motion.div>
             </div>
         </div>
     </div>
