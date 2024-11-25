546bcc9b10a08ff1ecce6606bb41ba3eb1eed5ba
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2024-11-25 16:48:47 -0400

    chore: add frame motion to experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,20 +1,39 @@
 import { EXPERIENCES } from "../constants"
+import { motion } from 'framer-motion'
 
 const Experience = () => {
     return (
         <div className="pb-4">
-            <h2 className="my-20 text-center text-4xl">Experience</h2>
+            <motion.h2
+                className="my-20 text-center text-4xl"
+                whileInView={{ opacity: 1, y: 0 }}
+                initial={{ opacity: 0, y: -100 }}
+                transition={{ duration: 1 }}
+
+            >
+                Experience
+            </motion.h2>
 
             <div>
                 {EXPERIENCES.map((experience, i) => (
                     <div key={i} className="mb-8 flex flex-wrap lg:justify-center">
-                        <div className="w-full lg:w-1/4">
+                        <motion.div 
+                            className="w-full lg:w-1/4"
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: -100 }}
+                            transition={{ duration: 1 }}
+                        >
                             <p className="mb-2 text-sm text-stone-400">
                                 {experience.year}
                             </p>
-                        </div>
+                        </motion.div>
 
-                        <div className="w-full max-w-xl lg:w-3/4">
+                        <motion.div 
+                            className="w-full max-w-xl lg:w-3/4"
+                            whileInView={{ opacity: 1, x: 0 }}
+                            initial={{ opacity: 0, x: 100 }}
+                            transition={{ duration: 1 }}
+                        >
                             <h3 className="mb-2 font-semibold">
                                 {experience.role} -{" "}
                                 <span className="text-sm text-stone-500">
@@ -28,7 +47,7 @@ const Experience = () => {
                                     {tech}
                                 </span>
                             ))}
-                        </div>
+                        </motion.div>
                     </div>
                 ))}
             </div>
