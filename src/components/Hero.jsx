e5bd3557328301baf118b5fb3986a259cabbb1ed
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2024-12-20 15:48:18 -0400

    chore: test to download curriculum

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -59,7 +59,7 @@ const Hero = () => {
             </motion.span>
 
             <motion.a
-              href="/src/assets/kaducurriculum.pdf"
+              href="/public/kaducurriculum.pdf"
               target="_blank"
               rel="noopener noreferrer"
               variants={childVariants}
