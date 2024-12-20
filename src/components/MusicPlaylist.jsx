abc141e77f53efc73dba4159aff91c481d27bfc2
Karlos Eduardo Plentis furtado <karlosfurtado@fitec.org.br>
2024-12-19 23:04:13 -0400

    delelte: remove unuse code

--- src/components/MusicPlaylist.jsx
+++ src/components/MusicPlaylist.jsx
@@ -1,43 +1,6 @@
-import { useEffect } from "react";
 import { BsThreeDots } from "react-icons/bs";
 
 const MusicPlaylist = () => {
-  const clientId = "e1a154e7c82044aeaab59f942ba4f734";
-  const redirectUri = "https://karlosplentis.vercel.app/";
-  const scopes = [
-    "streaming",
-    "user-read-email",
-    "user-read-private",
-    "user-read-playback-state",
-    "user-modify-playback-state",
-    "user-read-currently-playing",
-  ];
-
-  const handleSpotifyLogin = () => {
-    const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(
-      redirectUri
-    )}&scope=${encodeURIComponent(scopes.join(" "))}`;
-    window.location.href = authUrl;
-  };
-
-  const getTokenFromUrl = () => {
-    const hash = window.location.hash;
-    const token = hash
-      .substring(1)
-      .split("&")
-      .find((item) => item.startsWith("access_token"))
-      ?.split("=")[1];
-    return token;
-  };
-
-  useEffect(() => {
-    const token = getTokenFromUrl();
-    if (token) {
-      console.log("Spotify Token:", token);
-      window.location.hash = "";
-    }
-  }, []);
-
   return (
     <div className="flex items-center justify-center">
       <div className="my-20 flex flex-col rounded-2xl border-0 bg-black p-4 lg:w-3/4">
@@ -59,13 +22,6 @@ const MusicPlaylist = () => {
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
         />
-
-        <button
-          onClick={handleSpotifyLogin}
-          className="mt-4 rounded bg-green-500 px-6 py-2 text-black hover:bg-green-600"
-        >
-          Ouça músicas completas
-        </button>
       </div>
     </div>
   );
