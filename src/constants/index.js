0b435be7fde291dbab708de1fa1b22214562d9d0
Karlos Plentis <kaduplentis@gmail.com>
2025-11-05 19:07:49 -0400

    chore: comment basic projects from portfolio

--- src/constants/index.js
+++ src/constants/index.js
@@ -15,29 +15,29 @@ export const PROJECTS = [
     technologies: ["React", "NextJs", "SASS", "Typescript", "Framer Motion"],
     url: "https://spiderverse-rouge.vercel.app/",
   },
-  {
-    title: "CRUD",
-    image: crud,
-    description:
-      "CRUD application built with React and Firebase to deepen my understanding of full-stack development. Although the backend is currently offline, feel free to explore the codebase on GitHub.",
-    technologies: [
-      "React",
-      "NextJs",
-      "Eslint",
-      "TailwindCss",
-      "Typescript",
-      "Javascript",
-    ],
-    url: "https://github.com/Kadu1811/CRUD",
-  },
-  {
-    title: "Spotify Clone",
-    image: spotify_clone,
-    description:
-      "Responsive Spotify clone crafted to practice Bootstrap, HTML, and CSS. Features a sleek interface inspired by the original design. Perfect for showcasing my UI/UX design skills.",
-    technologies: ["HTML", "CSS", "Bootstrap"],
-    url: "https://kadu1811.github.io/SpotifyClone/index.html",
-  },
+  // {
+  //   title: "CRUD",
+  //   image: crud,
+  //   description:
+  //     "CRUD application built with React and Firebase to deepen my understanding of full-stack development. Although the backend is currently offline, feel free to explore the codebase on GitHub.",
+  //   technologies: [
+  //     "React",
+  //     "NextJs",
+  //     "Eslint",
+  //     "TailwindCss",
+  //     "Typescript",
+  //     "Javascript",
+  //   ],
+  //   url: "https://github.com/Kadu1811/CRUD",
+  // },
+  // {
+  //   title: "Spotify Clone",
+  //   image: spotify_clone,
+  //   description:
+  //     "Responsive Spotify clone crafted to practice Bootstrap, HTML, and CSS. Features a sleek interface inspired by the original design. Perfect for showcasing my UI/UX design skills.",
+  //   technologies: ["HTML", "CSS", "Bootstrap"],
+  //   url: "https://kadu1811.github.io/SpotifyClone/index.html",
+  // },
   {
     title: "Rescue Game",
     image: rescue_game,
@@ -54,22 +54,22 @@ export const PROJECTS = [
     technologies: ["HTML", "CSS", "Javascript"],
     url: "https://kadu1811.github.io/DinoGame/",
   },
-  {
-    title: "Calculator",
-    image: calculator,
-    description:
-      "A simple and intuitive calculator to help you with everyday calculations. Fully functional and user-friendly—try it out now!",
-    technologies: ["HTML", "CSS", "Javascript"],
-    url: "https://kadu1811.github.io/Calculator/",
-  },
-  {
-    title: "First Portfolio",
-    image: first_portfolio,
-    description:
-      "My first-ever portfolio project, built with HTML and CSS. A creative playground where I let my imagination and design skills shine for the first time.",
-    technologies: ["HTML", "CSS"],
-    url: "https://kadu1811.github.io/MeuPortfolio/",
-  },
+  // {
+  //   title: "Calculator",
+  //   image: calculator,
+  //   description:
+  //     "A simple and intuitive calculator to help you with everyday calculations. Fully functional and user-friendly—try it out now!",
+  //   technologies: ["HTML", "CSS", "Javascript"],
+  //   url: "https://kadu1811.github.io/Calculator/",
+  // },
+  // {
+  //   title: "First Portfolio",
+  //   image: first_portfolio,
+  //   description:
+  //     "My first-ever portfolio project, built with HTML and CSS. A creative playground where I let my imagination and design skills shine for the first time.",
+  //   technologies: ["HTML", "CSS"],
+  //   url: "https://kadu1811.github.io/MeuPortfolio/",
+  // },
 ];
 
 export const EXPERIENCES = [
